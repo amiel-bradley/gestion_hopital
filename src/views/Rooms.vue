e00ed3cd9bde919641cf96d4303f5a365384d547
Marcel <agueroyessia@gmail.com>
2026-02-16 21:46:05 +0100

    gestion rdv 100% + liste des rdv pris

--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -1,21 +1,37 @@
 <script setup>
-
+import { rooms } from '@/services/data';
 
 </script>
 
 
 <template>
-
-<p>Rooms</p>
+    <h2>Liste des chambres</h2>
+    <div class="container">
+        <!-- <p>Rooms</p> -->
+        <div v-for="room in rooms" :key="room.numero" class="room-card">
+            <p><strong>Chambre n° :</strong> {{ room.numero }}</p>
+            <p><strong>Capacité :</strong> {{ room.capacite }}</p>
+            <p><strong>Statut : </strong>
+                <span :class="room.statut">{{ room.statut }}</span></p>
+            <p><strong>Patients affectés :</strong> {{ room.affectationPatient }}</p>
+            <hr>
+        </div>
+    </div>
 
 </template>
 
 
 <style scoped>
-*{
+* {
     box-sizing: border-box;
-    margin:  0;
+    margin: 0;
     padding: 0;
 }
-
+.room-card {
+  border: 1px solid #ddd;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 8px;
+}
+.available { color: green; }
 </style>
